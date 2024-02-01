3fafc08112a0038569726a65316d93d976e188dd
smeet patel <smeet.posimyth@gmail.com>
2024-02-01 17:02:18 +0530

    some changes for testing the CI/CD pipeline

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import DisplayLocation from "./DisplayLocations.js/DisplayLocation";
 export default function App() {
   return (
     <div className="main">
-      <h2> Apollo-client Integration. </h2>
+      <h2>GraphQL learning and testing the CI/CD Pipeline.</h2>
       <DisplayLocation />
     </div>
   );
